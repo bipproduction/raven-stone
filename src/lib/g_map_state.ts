8f1ac06bd7fdc5ac2b5354d46a792773ef4bf776
bipproduction <bip.production.js@gmail.com>
2023-03-16 11:53:15 +0800

    fix seeder date di client

--- src/lib/g_map_state.ts
+++ src/lib/g_map_state.ts
@@ -3,5 +3,5 @@ import { DateValue } from '@mantine/dates'
 import moment from 'moment'
 export const glistCandidate = hookstate<any[]>([])
 export const gSelectedCandidate = hookstate<string>("1")
-export const gSelectedDate = hookstate<string>("2023-03-15")
+export const gSelectedDate = hookstate<string>("2023-03-16")
 export const gListKabupaten = hookstate<any[]>([])
