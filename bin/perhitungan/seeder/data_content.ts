2f43ebd6177819a9a11e3f4391f77906f07e19b8
bipproduction <bip.production.js@gmail.com>
2023-03-20 20:18:26 +0800

    update fix seeder

--- bin/perhitungan/seeder/data_content.ts
+++ bin/perhitungan/seeder/data_content.ts
@@ -2,20 +2,32 @@ import client from '@/lib/prisma_db'
 import data_emotion_v2 from './../data_emotion_v2.json'
 
 export const seederDataContent = async () => {
-    const dataEmotionV2 = data_emotion_v2
-    await client.dataByContent.deleteMany({
-        where: {
-            id: {
-                gt: 0
-            }
-        }
-    })
-    await client.dataByContent.createMany({
-        data: dataEmotionV2.map((v) => ({
-            ...v,
-            date: new Date("2023-03-16")
-        })),
-        skipDuplicates: true
-    })
+    // const dataEmotionV2 = data_emotion_v2
+    // await client.dataByContent.deleteMany({
+    //     where: {
+    //         id: {
+    //             gt: 0
+    //         }
+    //     }
+    // })
+    // await client.dataByContent.createMany({
+    //     data: dataEmotionV2.map((v) => ({
+    //         ...v,
+    //         date: new Date("2023-03-16")
+    //     })),
+    //     skipDuplicates: true
+    // })
+
+    for (let itm of data_emotion_v2) {
+        await client.dataByContent.upsert({
+            where: {
+                id: Number(itm.id)
+            },
+            update: itm,
+            create: itm
+        })
+    }
+
+    console.log("seeder datacontent success")
     return true
 }
--- bin/perhitungan/seeder/emotion.ts
+++ bin/perhitungan/seeder/emotion.ts
@@ -3,15 +3,31 @@ import emotion from './../emotion.json'
 import candidate from './../candidate.json'
 
 export const seederEmotion = async () => {
-    await client.emotion.deleteMany({ where: { id: { gt: 0 } } })
-    const dataEmotion: any[] = emotion
-    await client.emotion.createMany({
-        data: dataEmotion.map((v) => ({
-            id: Number(v.id),
-            name: v.name
-        })),
-        skipDuplicates: true
-    })
+    // await client.emotion.deleteMany({ where: { id: { gt: 0 } } })
+    // const dataEmotion: any[] = emotion
+    // await client.emotion.createMany({
+    //     data: dataEmotion.map((v) => ({
+    //         id: Number(v.id),
+    //         name: v.name
+    //     })),
+    //     skipDuplicates: true
+    // })
+
+    for (let itm of emotion) {
+        await client.emotion.upsert({
+            where: {
+                id: Number(itm.id)
+            },
+            update: {
+                name: itm.name
+            },
+            create: {
+                name: itm.name
+            }
+        })
+    }
+
+    console.log("seeder emotion success")
 
     return true
 }
