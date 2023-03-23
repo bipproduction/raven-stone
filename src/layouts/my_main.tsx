2101e521c58ef69edd29cecc58b42b0528ee656a
bipproduction <bip.production.js@gmail.com>
2023-03-23 11:10:30 +0800

    tambah login

--- src/layouts/my_main.tsx
+++ src/layouts/my_main.tsx
@@ -1,18 +1,44 @@
-
 import {
   MdAllInbox,
   MdBarChart,
   MdChat,
   MdGridView,
   MdMessage,
+  MdSecurityUpdate,
+  MdSecurityUpdateGood,
+  MdShelves,
+  MdSystemSecurityUpdate,
+  MdVerifiedUser,
+  MdWifiProtectedSetup,
 } from "react-icons/md";
+import {} from "react-icons/fa";
 import toast from "react-simple-toasts";
-import { ActionIcon, Box, Stack, Text, Tooltip } from "@mantine/core";
+import {
+  ActionIcon,
+  BackgroundImage,
+  Box,
+  Button,
+  Center,
+  Flex,
+  Group,
+  Paper,
+  PasswordInput,
+  ScrollArea,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+  Tooltip,
+} from "@mantine/core";
 import { useState } from "react";
 import Summary from "./summary/summary";
 import Medialistener from "./media_listener/media_listener";
 import PredictiveAi from "./prodictive_ai/prodictive_ai";
 import { gradient } from "@/styles/gradient";
+import Link from "next/link";
+import { useInputState } from "@mantine/hooks";
+import { api } from "@/lib/api";
+import { gIsUser } from "@/g_state/g_user_id";
 
 const listmenu = [
   {
@@ -49,15 +75,73 @@ const listContent = [
 
 const MyMain = () => {
   const [selectedMenu, setSelectedMenu] = useState<string>("1");
+  const [email, setEmail] = useInputState("");
+  const [password, setPassword] = useInputState("");
   return (
     <>
       <Stack>
-        {listContent.map((v) => (
-          <Box hidden={!(selectedMenu == v.id)} key={v.id}>
-            {<v.widget />}
-          </Box>
-        ))}
-        
+        <BackgroundImage src="/imgMedia/bg-3.png">
+          <ScrollArea>
+            <Center h={"100vh"}>
+              <Stack justify={"center"}>
+                <Title c={"indigo"}>EAGLE EYE PROJECT</Title>
+                <Paper p={"md"}>
+                  <Stack>
+                    <Title>LOGIN</Title>
+                    <TextInput
+                      placeholder="email"
+                      value={email}
+                      onChange={setEmail}
+                    />
+                    <PasswordInput
+                      placeholder="password"
+                      value={password}
+                      onChange={setPassword}
+                    />
+                    <Group>
+                      <MdVerifiedUser color="green" />
+                      <Text color={"green"}>Secure Access</Text>
+                    </Group>
+                    <Button
+                      bg={"indigo"}
+                      onClick={() => {
+                        const body = {
+                          email,
+                          password,
+                        };
+
+                        fetch(api.apiAuthLogin, {
+                          method: "POST",
+                          headers: {
+                            "Content-Type": "application/json",
+                          },
+                          body: JSON.stringify(body),
+                        }).then(async (res) => {
+                          if (res.status === 200) {
+                            const data = await res.json();
+                            localStorage.setItem("user_id", data.userId);
+                            toast("success");
+                            gIsUser.set(true);
+                          } else {
+                            toast("wrong email or password");
+                          }
+                        });
+                      }}
+                    >
+                      LOGIN
+                    </Button>
+                  </Stack>
+                </Paper>
+              </Stack>
+            </Center>
+            <Flex pos={"absolute"} bottom={0} left={0} gap={"md"} p={"md"}>
+              <Text>Bip Production @2023</Text>
+              <Text>Version: 2.0.1</Text>
+              <Text>build: 10453</Text>
+              <Link href={""}>Term Of Service</Link>
+            </Flex>
+          </ScrollArea>
+        </BackgroundImage>
       </Stack>
     </>
   );
