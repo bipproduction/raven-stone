ca590f7a6dcf2829a73991aadbf9049756656f07
bipproduction <bip.production.js@gmail.com>
2023-03-26 01:18:13 +0800

    update status code

--- src/pages/api/seeder/seeder-user.ts
+++ src/pages/api/seeder/seeder-user.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { seederUser } from './../../../../bin/perhitungan/seeder/seeder_user';
 const apiSeederUser = async (req: NextApiRequest, res: NextApiResponse) => {
     const success = await seederUser()
-    res.status(201).json({ success })
+    res.status(200).json({ success })
 }
 
 export default apiSeederUser
