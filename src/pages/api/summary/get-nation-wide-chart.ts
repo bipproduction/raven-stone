ddef34d8e608191547ba2b907c68c85e12e0ee6f
bipproduction <bip.production.js@gmail.com>
2023-03-25 21:25:30 +0800

    tambahan detai di detail city

--- src/pages/api/summary/get-nation-wide-chart.ts
+++ src/pages/api/summary/get-nation-wide-chart.ts
@@ -1,7 +1,18 @@
 import client from '@/lib/prisma_db';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+
+// Joy
+// Trust
+// Fear
+// Surprise
+// Sadness
+// Disgust
+// Anger
+// Anticipation
 const getNationWideChart = async (req: NextApiRequest, res: NextApiResponse) => {
     const data = await client.nationWideChart.findMany()
+    // console.log(data)
     res.status(200).json(data)
 }
 
