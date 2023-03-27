b6de405bf582bbca8c318af492b762faa9ffbb1c
bipproduction <bip.production.js@gmail.com>
2023-03-27 22:14:28 +0800

    auto commit

--- src/fun_load/func_load_nation_wide_rating.ts
+++ src/fun_load/func_load_nation_wide_rating.ts
@@ -1,9 +1,10 @@
+import { gSelectedDate } from '@/g_state/g_map_state';
 import { gNationWideRating } from "@/g_state/nation_wide_rating/g_nation_wide_rating";
 import { gSelectedCandidate1 } from "@/g_state/nation_wide_rating/g_selected_candidate1";
 import { gSelectedCandidate2 } from "@/g_state/nation_wide_rating/g_selected_candidate2";
 import { api } from "@/lib/api";
 
 export const funcLoadNationWideRating = () => 
-fetch(api.apiPredictiveAiNationWideRating + `?target1=${gSelectedCandidate1.value}&target2=${gSelectedCandidate2.value}`)
+fetch(api.apiPredictiveAiNationWideRating + `?date=${gSelectedDate.value}&candidate1=${gSelectedCandidate1.value}&candidate2=${gSelectedCandidate2.value}`)
 .then(v => v.json())
 .then(gNationWideRating.set)
