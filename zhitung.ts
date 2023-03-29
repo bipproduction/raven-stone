bac3d2b19fdb98512060057fec0e604d8d9c025e
bipproduction <bip.production.js@gmail.com>
2023-03-30 00:44:57 +0800

    auto commit

--- zhitung.ts
+++ zhitung.ts
@@ -32,6 +32,42 @@ const getProvinceValue = async () => {
     return pro
 }
 
+const main = () => {
+    const nilai_total = 8773;
+    const data_item = [
+      { name: 'pendidikan', value: 8773 },
+      { name: 'infrastruktur', value: 12085 },
+      { name: 'layanan_kesehatan', value: 642 },
+      { name: 'keagamaan', value: 126763 },
+      { name: 'kemiskinan', value: 119291 },
+      { name: 'lapangan_pekerjaan', value: 30329 },
+      { name: 'keadilan_sosial', value: 53391 }
+    ];
+    
+    // Hitung total nilai dari data_item
+    let total_value = 0;
+    for (let i = 0; i < data_item.length; i++) {
+      total_value += data_item[i].value;
+    }
+    
+    // Hitung proporsi pengurangan untuk setiap nilai dari data_item
+    let decrease_prop = (total_value - nilai_total) / total_value;
+    
+    // Kurangi nilai dari setiap data_item dengan proporsi yang sesuai
+    for (let i = 0; i < data_item.length; i++) {
+      let current_value = data_item[i].value;
+      let decrease_value = current_value * decrease_prop;
+      let new_value = Math.round(current_value - decrease_value);
+      data_item[i].value = new_value;
+      total_value -= decrease_value;
+    }
+    
+    console.log(_.sum(data_item.map((v) =>v.value))); // Hasil akhir dari data_item yang sudah diolah
+    
+}
+
+main()
+
 
 
 export { }
