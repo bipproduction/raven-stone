8bd211124f243197ebba93cce9f4140b7dbd8ea5
bipproduction <bip.production.js@gmail.com>
2023-04-04 23:46:07 +0800

    auto commit

--- src/layouts/sambutan.tsx
+++ src/layouts/sambutan.tsx
@@ -1,6 +1,5 @@
-
 import { sSelectedView } from "@/s_state/s_selected_view";
-import { Paper, Stack, Text, Title } from "@mantine/core";
+import { Box, Flex, Image, Paper, Stack, Text, Title } from "@mantine/core";
 
 const Sambutan = () => {
   return (
@@ -8,13 +7,18 @@ const Sambutan = () => {
       {(sSelectedView.value == "Top 10 Province By Emotions" ||
         sSelectedView.value == "Top 10 District by Emotions") && (
         <Paper shadow={"md"}>
+          <Flex>
+            <Box p={'md'}>
+            <Image src={"/icon_robot.svg"} alt={"icon"} width={54} />
+            </Box>
             <Stack p={"xs"} spacing={0}>
-          <Title c={"cyan.4"}>Hi Mr. Chusni</Title>
-          <Text c={"gray"}>
-            Welcome to Prabowo Subianto for President 2024 - Digital
-            Intelligence Winning Program.
-          </Text>
-        </Stack>
+              <Title c={"cyan.4"}>Hi Mr. Chusni</Title>
+              <Text c={"gray"}>
+                Welcome to Prabowo Subianto for President 2024 - Digital
+                Intelligence Winning Program.
+              </Text>
+            </Stack>
+          </Flex>
         </Paper>
       )}
     </>
--- src/layouts/summary/top_10_province_by_conversation.tsx
+++ src/layouts/summary/top_10_province_by_conversation.tsx
@@ -21,12 +21,16 @@ import { sTop10Province } from "@/s_state/s_top_10_province";
 import { sTop10ProvinceTake } from "@/s_state/s_top_10_province_take";
 import {
   ActionIcon,
+  Avatar,
   Box,
+  Center,
   Flex,
   Group,
+  HoverCard,
   Image,
   Pagination,
   Paper,
+  ScrollArea,
   Select,
   Stack,
   Table,
@@ -34,11 +38,43 @@ import {
   Title,
 } from "@mantine/core";
 import { useForceUpdate, useShallowEffect } from "@mantine/hooks";
-import { Avatar } from "antd";
 import _ from "lodash";
 import { useState } from "react";
+import { MdEmojiEmotions } from "react-icons/md";
 import PageTitle from "../page_title";
 import Sambutan from "../sambutan";
+import { AnimationOnScroll } from "react-animation-on-scroll";
+import { listAnimation } from "@/styles/styles_animation";
+import AnimateCssReact from "animate-css-reactjs";
+
+const HoverInfo = ({ text }: { text: string }) => {
+  return (
+    <>
+      <HoverCard>
+        <HoverCard.Target>
+          <ActionIcon
+            sx={{
+              border: "2px solid white",
+              borderRadius: "100px",
+            }}
+          >
+            <Image src={"/icon_robot.svg"} alt={"icon"} width={24} />
+          </ActionIcon>
+        </HoverCard.Target>
+        <HoverCard.Dropdown>
+          <Stack>
+            <Center>
+              <AnimateCssReact animation="bounce">
+                <Image width={100} src={"/icon_robot.svg"} alt={"icon"} />
+              </AnimateCssReact>
+            </Center>
+            <Text>{text}</Text>
+          </Stack>
+        </HoverCard.Dropdown>
+      </HoverCard>
+    </>
+  );
+};
 
 const Top10ProvinceByConversation = () => {
   const pageSize = 10;
@@ -60,7 +96,7 @@ const Top10ProvinceByConversation = () => {
           }}
         >
           <Group position="apart" py={"lg"}>
-            <Paper p={"md"} bg={stylesGradientBlueWhite} shadow={"md"}>
+            <Paper p={"md"} bg={stylesGradientBlueWhite} shadow={"md"} w={300}>
               <Stack>
                 <Flex>
                   {[
@@ -74,7 +110,7 @@ const Top10ProvinceByConversation = () => {
                         size={63}
                         onClick={async () => {
                           // gSelectedCandidate.set(v!.id.toString());
-                          sSelectedCandidate.value = v!.id.toString()
+                          sSelectedCandidate.value = v!.id.toString();
                           await funcLoadTop10District();
                           await funcLoadTop10Province();
                           update();
@@ -98,16 +134,19 @@ const Top10ProvinceByConversation = () => {
                     </Box>
                   ))}
                 </Flex>
-                <Title color={"blue.1"} order={3}>
-                  {
-                    sCandidate.value.find(
-                      (v) => v.id == Number(sSelectedCandidate.value)
-                    )?.name
-                  }
-                </Title>
+                <Flex justify="space-between" align={"end"}>
+                  <Title color={"blue.1"} order={3}>
+                    {
+                      sCandidate.value.find(
+                        (v) => v.id == Number(sSelectedCandidate.value)
+                      )?.name
+                    }
+                  </Title>
+                  <HoverInfo text="klik untuk melihat data kandidat" />
+                </Flex>
               </Stack>
             </Paper>
-            <Group position="right">
+            <Group position="right" align={"end"}>
               <Select
                 placeholder={sSelectedEmotion.value}
                 variant={"filled"}
@@ -120,20 +159,145 @@ const Top10ProvinceByConversation = () => {
                 onChange={async (val) => {
                   if (val) {
                     // sSelectedEmotion.set(val);
-                    sSelectedEmotion.value = val
+                    sSelectedEmotion.value = val;
                     await funcLoadTop10District();
                     await funcLoadTop10Province();
                     update();
                   }
                 }}
               />
+              <HoverCard>
+                <HoverCard.Target>
+                  <ActionIcon
+                    sx={{
+                      border: "2px solid white",
+                      borderRadius: "100px",
+                    }}
+                  >
+                    <Image src={"/icon_robot.svg"} alt={"icon"} width={24} />
+                  </ActionIcon>
+                </HoverCard.Target>
+                <HoverCard.Dropdown>
+                  <Stack>
+                    <Center>
+                      <AnimateCssReact animation="bounce">
+                        <Image
+                          width={100}
+                          src={"/icon_robot.svg"}
+                          alt={"icon"}
+                        />
+                      </AnimateCssReact>
+                    </Center>
+                    <Text>Pilih atau sort Emotion Sesuai kebutuhan</Text>
+                  </Stack>
+                </HoverCard.Dropdown>
+              </HoverCard>
             </Group>
           </Group>
           <Stack>
-            <Table highlightOnHover verticalSpacing={"md"}>
+            <Group position="right">
+              <HoverCard>
+                <HoverCard.Target>
+                  <ActionIcon
+                    size={32}
+                    radius={100}
+                    sx={{
+                      border: "2px solid white",
+                      borderRadius: "100px",
+                    }}
+                  >
+                    <Image width={24} src={"/icon_robot.svg"} alt={"icon"} />
+                  </ActionIcon>
+                </HoverCard.Target>
+                <HoverCard.Dropdown>
+                  <ScrollArea h={300}>
+                    <Stack w={500}>
+                      <Center>
+                        <AnimateCssReact animation="bounce">
+                          <Image
+                            width={100}
+                            src={"/icon_robot.svg"}
+                            alt={"icon"}
+                          />
+                        </AnimateCssReact>
+                      </Center>
+                      <Text>
+                        TRUST (Kepercayaan): Jika seseorang merasa kepercayaan
+                        terhadap seseorang atau figure, maka ia merasa yakin
+                        bahwa orang atau figure tersebut dapat diandalkan dan
+                        memiliki kualitas yang baik, sehingga orang tersebut
+                        merasa nyaman dan aman berada di dekat mereka.
+                      </Text>
+                      <Text>
+                        JOY (Kepuasan): Jika seseorang merasa sukacita terhadap
+                        seseorang atau figure, maka ia merasa senang, gembira,
+                        dan bahagia ketika berada di dekat mereka. Hal ini bisa
+                        terjadi karena seseorang merasa diperhatikan, dihargai,
+                        atau merasa terhibur.
+                      </Text>
+                      <Text>
+                        SURPRISE (Kejutan): Jika seseorang merasa kejutan
+                        terhadap seseorang atau figure, maka ia merasa senang
+                        dan terkesan dengan peristiwa atau aksi yang tidak
+                        terduga. Hal ini bisa terjadi ketika seseorang
+                        mendapatkan hadiah yang tidak ia duga dari seseorang
+                        atau figure yang ia sukai.
+                      </Text>
+                      <Text>
+                        ANTICIPATION (Antisipasi): Jika seseorang merasa
+                        antisipasi terhadap seseorang atau figure, maka ia
+                        merasa sangat menantikan momen atau kejadian tertentu
+                        bersama mereka. Hal ini bisa terjadi ketika seseorang
+                        merasa sangat menantikan pertemuan atau kegiatan bersama
+                        orang atau figure yang mereka sukai.
+                      </Text>
+                      <Text>
+                        SADNESS (Kesedihan): Jika seseorang merasa kesedihan
+                        terhadap seseorang atau figure, maka ia merasa sedih
+                        atau kecewa karena suatu peristiwa atau kondisi yang
+                        tidak menyenangkan terjadi pada mereka atau pada orang
+                        atau figure yang mereka sukai.
+                      </Text>
+                      <Text>
+                        FEAR (Ketakutan): Jika seseorang merasa ketakutan
+                        terhadap seseorang atau figure, maka ia merasa takut
+                        atau cemas terhadap sesuatu yang mungkin terjadi pada
+                        mereka atau pada orang atau figure yang mereka sukai.
+                      </Text>
+                      <Text>
+                        ANGER (Kemarahan): Jika seseorang merasa kemarahan
+                        terhadap seseorang atau figure, maka ia merasa marah
+                        atau tidak puas terhadap suatu peristiwa atau kondisi
+                        yang tidak baik terjadi pada mereka atau pada orang atau
+                        figure yang mereka sukai.
+                      </Text>
+                      <Text>
+                        DISGUST (Jijik): Jika seseorang merasa jijik terhadap
+                        seseorang atau figure, maka ia merasa tidak suka atau
+                        tidak nyaman terhadap suatu peristiwa atau kondisi yang
+                        terkait dengan orang atau figure yang mereka sukai.
+                      </Text>
+                    </Stack>
+                  </ScrollArea>
+                </HoverCard.Dropdown>
+              </HoverCard>
+            </Group>
+            <Table verticalSpacing={"md"} bg={stylesGradient1}>
               <thead>
                 <tr>
-                  {Object.keys(_.omit(sTop10ProvinceTake.value[0], "id")).map(
+                  <th>NO</th>
+                  <th>CITY</th>
+                  <th>VALUE</th>
+                  <th>TRUST</th>
+                  <th>JOY</th>
+                  <th>SURPRISE</th>
+                  <th>ANTICIPATION</th>
+                  <th>SADNESS</th>
+                  <th>FEAR</th>
+                  <th>ANGER</th>
+                  <th>DISGUST</th>
+
+                  {/* {Object.keys(_.omit(sTop10ProvinceTake.value[0], "id")).map(
                     (v, i) => (
                       <th key={i}>
                         <Text c={"cyan.8"} fw={"bold"}>
@@ -141,13 +305,90 @@ const Top10ProvinceByConversation = () => {
                         </Text>
                       </th>
                     )
-                  )}
+                  )} */}
                 </tr>
               </thead>
               <tbody>
                 {sTop10ProvinceTake.value.map((v: any, i) => (
                   <tr key={i}>
-                    {Object.keys(_.omit(v, "id")).map((v2, ii) => (
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"white"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {v.no}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"white"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {v.name}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"white"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.value)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"green.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.trust)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"green.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.joy)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"green.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.surprise)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"gray.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.anticipation)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"pink.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.sadness)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"pink.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.fear)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"pink.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.anger)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    <td style={{ padding: 0 }}>
+                      <Paper bg={"pink.1"} p={"xs"}>
+                        <Text fw={"bold"} color={"gray"}>
+                          {Intl.NumberFormat("id-ID").format(v.disgust)}
+                        </Text>
+                      </Paper>
+                    </td>
+                    {/* {Object.keys(_.omit(v, "id")).map((v2, ii) => (
                       <td
                         style={{
                           backgroundColor: [
@@ -183,7 +424,7 @@ const Top10ProvinceByConversation = () => {
                           </Text>
                         </Box>
                       </td>
-                    ))}
+                    ))} */}
                   </tr>
                 ))}
               </tbody>
