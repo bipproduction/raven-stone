e6cf088aafbcb0c6577286ec08a11fd44b8f7fef
bipproduction <bip.production.js@gmail.com>
2023-04-05 10:44:11 +0800

    auto commit

--- src/pages/api/predictive-ai/contsextual_content_get.ts
+++ src/pages/api/predictive-ai/contsextual_content_get.ts
@@ -1,14 +1,15 @@
 import client from '@/lib/prisma_db';
+import _ from 'lodash';
 import { NextApiRequest, NextApiResponse } from 'next';
 const contextDirectiontetGet = async (req: NextApiRequest, res: NextApiResponse) => {
     const data = await client.contextualContent.findMany({
         orderBy: {
-            id: "desc"
+            id: "desc",
         }
     })
 
-    // console.log(data.data)
-    res.status(200).json(data)
+    const sort = _.orderBy(data, 'data.audiences', 'desc')
+    res.status(200).json(sort)
 }
 
 export default contextDirectiontetGet
