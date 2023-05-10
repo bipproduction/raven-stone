4e2920d806bad21929390ac258e5ec32f92059e5
bipproduction <bip.production.js@gmail.com>
2023-05-10 18:04:50 +0800

    fix copy data

--- src/pages/api/copy-data.ts
+++ src/pages/api/copy-data.ts
@@ -4,32 +4,144 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import _ from 'lodash';
 const copyData = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method == "POST") {
-        const { from, to, candidateId } = req.query
-        const data = await client.dataByContent.findMany({
+        const body = req.body
+
+        if (body.typeAction == "all") {
+            const dataNya = await client.dataByContent.findMany({
+                where: {
+                    date: new Date(body.dateFrom as any),
+                }
+            })
+
+            await client.dataByContent.deleteMany({
+                where: {
+                    date: new Date(body.dateTo as any)
+                }
+            })
+
+            const data = dataNya.map((v) => ({
+                ..._.omit(v, ['id']),
+                date: new Date(body.dateTo as any)
+            }))
+
+            // console.log(bahan)
+
+
+            await client.dataByContent.createMany({ data })
+
+            return res.status(201).json({
+                success: true
+            })
+        } else if (body.typeAction == "selected") {
+            const dataNya = await client.dataByContent.findMany({
+                where: {
+                    date: new Date(body.dateFrom as any),
+                    candidateId: Number(body.selectedCandidate)
+                }
+            })
+
+            await client.dataByContent.deleteMany({
+                where: {
+                    date: new Date(body.dateTo as any),
+                    candidateId: Number(body.selectedCandidate)
+                }
+            })
+
+            const data = dataNya.map((v) => ({
+                ..._.omit(v, ['id']),
+                date: new Date(body.dateTo as any)
+            }))
+
+            await client.dataByContent.createMany({ data })
+
+            return res.status(201).json({
+                success: true
+            })
+        } else if (body.typeAction == "one_for_all") {
+            const dataNya = await client.dataByContent.findMany({
+                where: {
+                    date: new Date(body.dateFrom as any),
+                    candidateId: Number(body.selectedCandidate)
+                }
+            })
+
+            await client.dataByContent.deleteMany({
+                where: {
+                    date: new Date(body.dateTo as any)
+                }
+            })
+
+            const allCandidate = await client.candidate.findMany()
+
+            for (let itm of allCandidate) {
+                const data = dataNya.map((v) => ({
+                    ..._.omit(v, ['id']),
+                    date: new Date(body.dateTo as any),
+                    candidateId: Number(itm.id)
+                }))
+                await client.dataByContent.createMany({ data })
+                console.log(`insert candidate ${itm.name}`)
+            }
+
+            return res.status(201).json({
+                success: true
+            })
+
+        }
+
+
+        const dataNya = await client.dataByContent.findMany({
             where: {
-                date: new Date(from as any)
+                date: new Date(body.dateFrom as any),
+                candidateId: Number(body.selectedCandidate)
             }
         })
-
-        const hasil = candidateId == "" ? data.map((v) => ({
-            ..._.omit(v, ['id']),
-            date: new Date(to as any)
-        })) : data.filter((v) => v.candidateId == Number(candidateId)).map((v) => ({
-            ..._.omit(v, ['id']),
-            date: new Date(to as any)
-        }))
 
         await client.dataByContent.deleteMany({
             where: {
-                date: new Date(to as any)
+                date: new Date(body.dateTo as any),
+                candidateId: Number(body.selectedCandidateOther)
             }
         })
 
-        await client.dataByContent.createMany({ data: hasil })
+        const data = dataNya.map((v) => ({
+            ..._.omit(v, ['id']),
+            date: new Date(body.dateTo as any),
+            candidateId: Number(body.selectedCandidateOther)
+        }))
 
-        // console.log(hasil)
+        await client.dataByContent.createMany({ data })
 
-        res.status(201).json({ success: true })
+        return res.status(201).json({
+            success: true
+        })
+
+        // const { from, to, candidateId } = req.query
+        // const data = await client.dataByContent.findMany({
+        //     where: {
+        //         date: new Date(from as any)
+        //     }
+        // })
+
+        // const hasil = candidateId == "" ? data.map((v) => ({
+        //     ..._.omit(v, ['id']),
+        //     date: new Date(to as any)
+        // })) : data.filter((v) => v.candidateId == Number(candidateId)).map((v) => ({
+        //     ..._.omit(v, ['id']),
+        //     date: new Date(to as any)
+        // }))
+
+        // await client.dataByContent.deleteMany({
+        //     where: {
+        //         date: new Date(to as any)
+        //     }
+        // })
+
+        // await client.dataByContent.createMany({ data: hasil })
+
+        // // console.log(hasil)
+
+        // res.status(201).json({ success: true })
     }
 }
 
