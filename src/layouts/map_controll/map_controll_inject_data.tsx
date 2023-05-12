8b5d688d47f2366a3a9a9b1a8dca9f66fb919349
bipproduction <bip.production.js@gmail.com>
2023-05-12 15:52:31 +0800

    tambahan

--- src/layouts/map_controll/map_controll_inject_data.tsx
+++ src/layouts/map_controll/map_controll_inject_data.tsx
@@ -20,12 +20,23 @@ import { useState } from "react";
 import _ from "lodash";
 import { api } from "@/lib/api";
 import { DateInput } from "@mantine/dates";
+
 const openModal = atomWithStorage("map_controll_inject_data_modal", false);
+const _list_data = atomWithStorage<any[] | undefined>(
+  "map_controll_inject_list_data",
+  undefined
+);
+const _type = atomWithStorage<string | undefined>(
+  "map_controll_inject_type",
+  undefined
+);
 
 export default function MapControllInjectData({ bg }: { bg: string }) {
   const [open, setOpen] = useAtom(openModal);
-  const [listData, setListData] = useState<any[]>();
-  const [type, setType] = useState<string>();
+  const [listData, setListData] = useAtom(_list_data);
+  const [type, setType] = useAtom(_type);
+  //   const [listData, setListData] = useState<any[]>();
+  //   const [type, setType] = useState<string>();
 
   function onSend() {
     const data = listData?.map((v) => ({
@@ -49,53 +60,7 @@ export default function MapControllInjectData({ bg }: { bg: string }) {
         closeOnClickOutside={false}
       >
         <Stack>
-          <Dropzone
-            accept={["text/csv"]}
-            onDrop={(file) => {
-              const name = file[0].name;
-              if (name) {
-                setType(
-                  name.includes("update")
-                    ? "update"
-                    : name.includes("insert")
-                    ? "insert"
-                    : undefined
-                );
-
-                const reader = new FileReader();
-                reader.readAsText(file[0]);
-
-                reader.onload = function (event: any) {
-                  const csv = event.target.result;
-                  // const data = PAPA.parse(csv);
-                  // console.log(data.data);
-                  PAPA.parse(csv, {
-                    header: true,
-                    skipEmptyLines: true,
-                    complete: function (results, file) {
-                      setListData(results.data);
-                    },
-                  });
-                };
-              }
-            }}
-            maxSize={3 * 1024 ** 2}
-          >
-            <Group position="center">
-              <Dropzone.Accept>
-                <MdUploadFile size={42} />
-              </Dropzone.Accept>
-              <div>
-                <Text size="xl" inline>
-                  Drag CSV here or click to select files
-                </Text>
-                <Text size="sm" color="dimmed" inline mt={7}>
-                  Attach as many files as you like, each file should not exceed
-                  5mb
-                </Text>
-              </div>
-            </Group>
-          </Dropzone>
+          <DropView />
           <Group>
             <Title>{type}</Title>
             <Flex>
@@ -108,35 +73,99 @@ export default function MapControllInjectData({ bg }: { bg: string }) {
               <Button>{type}</Button>
             </Flex>
           </Group>
-          <Stack>
-            {listData && (
-              <Stack>
-                <ScrollArea h={300}>
-                  {/* {JSON.stringify(listData)} */}
-                  <Table>
-                    <thead>
-                      <tr>
-                        {_.keys(listData[0]).map((v, i) => (
-                          <th key={i}>{v}</th>
-                        ))}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {listData.map((v, i) => (
-                        <tr key={i}>
-                          {_.values(v).map((vv, ii) => (
-                            <td key={ii}>{vv}</td>
-                          ))}
-                        </tr>
-                      ))}
-                    </tbody>
-                  </Table>
-                </ScrollArea>
-              </Stack>
-            )}
-          </Stack>
+          {listData && <TableView />}
         </Stack>
       </Modal>
+    </>
+  );
+}
+
+function DropView() {
+  const [listData, setListData] = useAtom(_list_data);
+  const [type, setType] = useAtom(_type);
+  return (
+    <>
+      <Dropzone
+        accept={["text/csv"]}
+        onDrop={(file) => {
+          const name = file[0].name;
+          if (name) {
+            setType(
+              name.includes("update")
+                ? "update"
+                : name.includes("insert")
+                ? "insert"
+                : undefined
+            );
+
+            const reader = new FileReader();
+            reader.readAsText(file[0]);
+
+            reader.onload = function (event: any) {
+              const csv = event.target.result;
+              // const data = PAPA.parse(csv);
+              // console.log(data.data);
+              PAPA.parse(csv, {
+                header: true,
+                skipEmptyLines: true,
+                complete: function (results, file) {
+                  setListData(results.data);
+                },
+              });
+            };
+          }
+        }}
+        maxSize={3 * 1024 ** 2}
+      >
+        <Group position="center">
+          <Dropzone.Accept>
+            <MdUploadFile size={42} />
+          </Dropzone.Accept>
+          <div>
+            <Text size="xl" inline>
+              Drag CSV here or click to select files
+            </Text>
+            <Text size="sm" color="dimmed" inline mt={7}>
+              Attach as many files as you like, each file should not exceed 5mb
+            </Text>
+          </div>
+        </Group>
+      </Dropzone>
+    </>
+  );
+}
+
+function TableView() {
+  const [listData, setListData] = useAtom(_list_data);
+  return (
+    <>
+      <Stack>
+        {listData && (
+          <Stack>
+            <ScrollArea h={300}>
+              {/* {JSON.stringify(listData)} */}
+              <Table>
+                <thead>
+                  <tr>
+                    {_.keys(listData[0]).map((v, i) => (
+                      <th key={i}>{v}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {listData.map((v, i) => (
+                    <tr key={i}>
+                      {_.values(v).map((vv, ii) => (
+                        <td key={ii}>{vv}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </ScrollArea>
+          </Stack>
+        )}
+      </Stack>
     </>
   );
 }
