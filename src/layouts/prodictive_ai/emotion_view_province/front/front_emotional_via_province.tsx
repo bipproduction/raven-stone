bcdc7038b501de188f6db08bc25ef9f336b51c58
bipproduction <bip.production.js@gmail.com>
2023-05-24 17:19:40 +0800

    apa

--- src/layouts/prodictive_ai/emotion_view_province/front/front_emotional_via_province.tsx
+++ src/layouts/prodictive_ai/emotion_view_province/front/front_emotional_via_province.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mantine/core";
+import { Box, Flex, Grid, Group, Paper, Stack } from "@mantine/core";
 import { ComSelectCandidate } from "./com/com_select_candidate";
 import PageTitle from "@/layouts/page_title";
 import { useShallowEffect } from "@mantine/hooks";
@@ -6,6 +6,7 @@ import { fun_load_emotion_province } from "./fun/fun_load_emotion_province";
 import { useAtom } from "jotai";
 import { val_selected_candidate } from "./val/val_seleced_candidate";
 import { val_list_emotion } from "./val/val_list_emotion";
+import { ComChartBar } from "./com/com_chart_bar";
 
 export const FrontEmotionalViewViaProvince = () => {
   const [candidateId, setCandidateId] = useAtom(val_selected_candidate);
@@ -20,9 +21,28 @@ export const FrontEmotionalViewViaProvince = () => {
   return (
     <>
       <Stack spacing={"lg"}>
-        {JSON.stringify(listEmotion)}
         <PageTitle title={"Emotion View Province"} />
         <ComSelectCandidate />
+        {listEmotion.map((v, i) => (
+          <Box key={i} p={"md"}>
+            <Paper p={"md"} shadow="md">
+              <Flex >
+                <ComChartBar lsData={v.emotion} />
+                <pre>
+                {JSON.stringify(v, null, 2)}
+                </pre>
+              </Flex>
+              {/* <Grid>
+                <Grid.Col span={"content"}>
+                  <ComChartBar lsData={v.emotion} />
+                </Grid.Col>
+                <Grid.Col span={"auto"}>
+                    {JSON.stringify(v)}
+                </Grid.Col>
+              </Grid> */}
+            </Paper>
+          </Box>
+        ))}
       </Stack>
     </>
   );
