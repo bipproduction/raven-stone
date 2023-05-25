1b42ecc2bd50f6ba66eaabc70e62f00cf3eff104
bipproduction <bip.production.js@gmail.com>
2023-05-25 19:24:06 +0800

    ya

--- src/layouts/prodictive_ai/emotion_view_province/api/api_leader_persona_by_province_id_get.ts
+++ src/layouts/prodictive_ai/emotion_view_province/api/api_leader_persona_by_province_id_get.ts
@@ -1,4 +1,5 @@
 import client from "@/lib/prisma_db";
+import _ from "lodash";
 
 export async function api_leader_persona_by_province_id_get(req: any, res: any) {
   const { provinceId } = req.query
@@ -16,29 +17,40 @@ export async function api_leader_persona_by_province_id_get(req: any, res: any) 
     }
   })
 
-
-
-  const mergedResult = data.reduce((result: any, item) => {
-   
-
-    try {
-      const content: any = item.CityLeaderPersonaPrediction[0].data;
-      // console.log(content)
-      content.content.forEach((element: any) => {
-        const existingItem: any = result.find((el: any) => el.title === element.title);
-        if (existingItem) {
-          existingItem.value += element.value;
-        } else {
-          result.push({ title: element.title, value: element.value });
-        }
-      });
-    } catch (error) {
-
+  const hasil = []
+  for (let itm of data) {
+    const d: any = itm.CityLeaderPersonaPrediction[0] ? itm.CityLeaderPersonaPrediction[0].data : null
+    if (d) {
+      hasil.push(d.content)
     }
-    return result;
-  }, []);
+  }
 
-  // console.log(mergedResult, null, 2)
+  let maka = {};
+  if (!_.isEmpty(hasil)) {
+    const flattenedData = _.flatten(hasil);
+    const totals = _.reduce(flattenedData, (result: any, item: any) => {
+      if (result[item.title]) {
+        result[item.title] += item.value;
+      } else {
+        result[item.title] = item.value;
+      }
 
-  return res.status(200).json(mergedResult)
+      console.log(result)
+      return result;
+    }, {});
+
+    const totalValues = _.sum(_.values(totals));
+
+    const percentages = _.mapValues(totals, (value) => {
+      return _.round((value / totalValues) * 100, 2);
+    });
+
+    maka = _.keys(percentages).map((v) => ({
+      title: v,
+      value: percentages[v]
+    }))
+  }
+
+
+  return res.status(200).json(maka)
 }
