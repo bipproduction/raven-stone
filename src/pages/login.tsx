618d31ec9322662bb8778f787f344191ce46b7e4
bipproduction <bip.production.js@gmail.com>
2023-06-10 12:12:49 +0800

    tambahan

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,16 +1,24 @@
+import { fDb } from "@/lib/fbs";
+import { sUser } from "@/s_state/s_user";
 import { Button, Group, Modal, Stack, Text } from "@mantine/core";
 import { useDidUpdate, useShallowEffect } from "@mantine/hooks";
 import { Model } from "echarts";
+import { onChildChanged, onValue, ref, set } from "firebase/database";
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
 import toast from "react-simple-toasts";
+
+const login_user = atomWithStorage("loginUser", "");
 
 const Testo = () => {
   const [data, setData] = useState("No result");
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
   const [open, setopen] = useState(false);
   const router = useRouter();
+  const [loginUser, setLoginUser] = useAtom(login_user);
 
   useDidUpdate(() => {
     requestCameraPermission();
@@ -26,11 +34,18 @@ const Testo = () => {
     }
   };
 
+  async function onKirimData() {
+    set(ref(fDb, `eagle_2/auth/qr/${data}`), {
+      id: "ini adalah idnya",
+    });
+  }
+
   return (
     <>
       <Group>
         <Stack>
           <Button onClick={() => setopen(true)}>Open</Button>
+          <Text>{sUser.value!.name}</Text>
           <Text>{data}</Text>
           <Modal
             opened={open}
@@ -39,7 +54,6 @@ const Testo = () => {
               router.reload();
             }}
           >
-
             <Text>Permisi</Text>
             {JSON.stringify(hasPermission)}
             <QrReader
@@ -50,7 +64,9 @@ const Testo = () => {
               onResult={(result, error) => {
                 if (result) {
                   setData(result.getText());
-                  setopen(false)
+                  setLoginUser(result.getText());
+                  setopen(false);
+                  onKirimData();
                 }
 
                 if (!!error) {
