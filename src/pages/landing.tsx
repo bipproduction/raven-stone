8a4fc9625b8a75732cf30f921e07347fd423e1a2
bipproduction <bip.production.js@gmail.com>
2023-06-12 10:46:21 +0800

    tambahannya

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,13 +1,17 @@
 import { fDb } from "@/lib/fbs";
 import {
+  ActionIcon,
   Box,
+  Button,
   Center,
   Flex,
   Grid,
   Group,
   Image,
   List,
+  LoadingOverlay,
   MediaQuery,
+  Overlay,
   Paper,
   Space,
   Stack,
@@ -15,99 +19,147 @@ import {
   Title,
 } from "@mantine/core";
 import { ListItem } from "@mantine/core/lib/List/ListItem/ListItem";
-import { useShallowEffect } from "@mantine/hooks";
+import { useInterval, useShallowEffect, useTimeout } from "@mantine/hooks";
 import { onChildChanged, ref } from "firebase/database";
+import { useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 import _ from "lodash";
 import { useState } from "react";
-import { MdSwapVerticalCircle } from "react-icons/md";
+import {
+  MdAccountCircle,
+  MdSearch,
+  MdSwapVerticalCircle,
+} from "react-icons/md";
 import Iframe from "react-iframe";
 import QRCode from "react-qr-code";
 import { v4 } from "uuid";
-const id = v4();
+// const id = v4();
+
+const val_user = atomWithStorage<any | null>("test_user", {});
+const val_qr = atomWithStorage<string | null>("test_qr_id", null);
 
 export default function Landing() {
-  const [user, setUser] = useState("");
+  const [user, setUser] = useAtom(val_user);
+  const [qrValue, setQrValue] = useAtom(val_qr);
+
+  const interval = useInterval(function () {
+    console.log("set qr value");
+    setQrValue(v4());
+  }, 5000);
+
+  useShallowEffect(() => {
+    setQrValue(v4());
+  }, []);
+
+  useShallowEffect(() => {
+    interval.start();
+    return interval.stop;
+  });
 
   useShallowEffect(() => {
     return onChildChanged(ref(fDb, `eagle_2/auth/`), (snapshot: any) => {
       if (snapshot) {
         const _list = _.keys(snapshot.val());
-        if (_list.includes(id)) {
-          setUser(snapshot.val()[id]);
-          console.log(snapshot.val()[id]);
+        if (_list.includes(qrValue!)) {
+          interval.stop();
+          setUser(snapshot.val()[qrValue!]);
+          console.log(snapshot.val()[qrValue!]);
         }
       }
     });
-  }, []);
+  }, [qrValue]);
+
+  if (!user || !user.name)
+    return (
+      <>
+        <Stack spacing={0}>
+          <Box h={200} bg={"teal"}></Box>
+          <Box h={800} bg={"gray.8"}></Box>
+          <Box pos={"absolute"} top={70} w={"100%"}>
+            <Center>
+              <Stack spacing={"lg"}>
+                <Group>
+                  <MdSwapVerticalCircle size={52} color="white" />
+                  <Title order={3} color="white">
+                    Eagle Eye
+                  </Title>
+                </Group>
+                <Paper maw={820} bg={"white"} py={"lg"}>
+                  <Flex wrap={"wrap"} p={"lg"}>
+                    <Stack spacing={"lg"} p={"lg"}>
+                      <Title c={"black"} order={3}>
+                        Gunakan Eagle Eye Di Komputer Anda
+                      </Title>
+                      <Text c={"black"}>
+                        1. Buka Eagle Eye Di Hanphone Anda
+                      </Text>
+                      <Text c={"black"}>2. Ketuk Tombol Login Web</Text>
+                      <Text c={"black"}>
+                        3. Arahkan Hanphone Ke Layar Ini Untuk Scan QrCode
+                      </Text>
+                    </Stack>
+                    <MediaQuery smallerThan={"sm"} styles={{ width: "100%" }}>
+                      <Flex justify={"center"}>
+                        <Box w={250} p={"lg"} pos={"relative"}>
+                          <QRCode
+                            size={250}
+                            style={{
+                              height: "auto",
+                              maxWidth: "100%",
+                              width: "100%",
+                            }}
+                            value={qrValue ?? ""}
+                            viewBox={`0 0 256 256`}
+                          />
+                        </Box>
+                      </Flex>
+                    </MediaQuery>
+                  </Flex>
+                  <Space h={100} />
+                  <Stack bg={"gray.1"} spacing={"lg"} p={"lg"} align="center">
+                    <Title c={"black"} order={3}>
+                      Tutorial
+                    </Title>
+                    <Text c={"black"}>Perlu Bantuan Untuk Pemula</Text>
+                    <Image
+                      src={
+                        "https://ebi.ai/wp-content/uploads/2022/05/QR1-1.png"
+                      }
+                      width={300}
+                      height={300}
+                      alt={"apa"}
+                    />
+                  </Stack>
+                </Paper>
+              </Stack>
+            </Center>
+          </Box>
+        </Stack>
+      </>
+    );
 
   return (
     <>
-      <Stack spacing={0}>
-        <Box h={200} bg={"teal"}></Box>
-        <Box h={800} bg={"gray.8"}></Box>
-        <Box pos={"absolute"} top={70} w={"100%"}>
-          <Center>
-            <Stack spacing={"lg"}>
+      {
+        <Group position="center">
+          <Paper p={"lg"}>
+            <Stack>
               <Group>
-                <MdSwapVerticalCircle size={52} color="white" />
-                <Title order={3} color="white">
-                  Eagle Eye
-                </Title>
+                <MdAccountCircle size={42} color="teal" />
+                <Title>{`Hi' ${user!.name}`}</Title>
               </Group>
-              <Paper maw={820} bg={"white"} py={"lg"}>
-                <Flex wrap={"wrap"} p={"lg"}>
-                  <Stack spacing={"lg"} p={"lg"}>
-                    <Title c={"black"} order={3}>
-                      Gunakan Eagle Eye Di Komputer Anda
-                    </Title>
-                    <Text c={"black"}>1. Buka Eagle Eye Di Hanphone Anda</Text>
-                    <Text c={"black"}>2. Ketuk Tombol Login Web</Text>
-                    <Text c={"black"}>
-                      3. Arahkan Hanphone Ke Layar Ini Untuk Scan QrCode
-                    </Text>
-                  </Stack>
-                  <MediaQuery smallerThan={"sm"} styles={{ width: "100%" }}>
-                    <Flex justify={"center"}>
-                      <Box w={250} p={"lg"}>
-                        <QRCode
-                          size={256}
-                          style={{
-                            height: "auto",
-                            maxWidth: "100%",
-                            width: "100%",
-                          }}
-                          value={id}
-                          viewBox={`0 0 256 256`}
-                        />
-                        <pre
-                          style={{
-                            color: "black",
-                          }}
-                        >
-                          {JSON.stringify(user, null, 2)}
-                        </pre>
-                      </Box>
-                    </Flex>
-                  </MediaQuery>
-                </Flex>
-                <Space h={100} />
-                <Stack bg={"gray.1"} spacing={"lg"} p={"lg"} align="center">
-                  <Title c={"black"} order={3}>
-                    Tutorial
-                  </Title>
-                  <Text c={"black"}>Perlu Bantuan Untuk Pemula</Text>
-                  <Image
-                    src={"https://ebi.ai/wp-content/uploads/2022/05/QR1-1.png"}
-                    width={300}
-                    height={300}
-                    alt={"apa"}
-                  />
-                </Stack>
-              </Paper>
+              <Button
+                color="orange"
+                onClick={() => {
+                  setUser(null);
+                }}
+              >
+                Logout
+              </Button>
             </Stack>
-          </Center>
-        </Box>
-      </Stack>
+          </Paper>
+        </Group>
+      }
     </>
   );
 }
