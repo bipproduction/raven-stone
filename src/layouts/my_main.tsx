e7dc0d8bbe190d9539c4cbd1278f8457e43f3f38
bipproduction <bip.production.js@gmail.com>
2023-08-26 10:20:23 +0800

    tambahan tutup login

--- src/layouts/my_main.tsx
+++ src/layouts/my_main.tsx
@@ -5,6 +5,8 @@ import {
   Button,
   Center,
   Group,
+  Loader,
+  Overlay,
   Paper,
   PasswordInput,
   ScrollArea,
@@ -28,6 +30,7 @@ import PredictiveAi from "./prodictive_ai/prodictive_ai";
 import Summary from "./summary/summary_derecated";
 // import { gUser } from "@/g_state/auth/g_user";
 import useTranslation from 'next-translate/useTranslation';
+import { useRouter } from "next/router";
 
 
 const listmenu = [
@@ -69,6 +72,7 @@ const MyMain = () => {
   const [password, setPassword] = useInputState("");
   const { t, lang } = useTranslation();
   const [dataTrans, setDataTrans] = useState("")
+  const [url, setUrl] = useState<string | null>(null)
 
   useShallowEffect(() => {
     fetch('/api/translate', {
@@ -92,17 +96,36 @@ const MyMain = () => {
     })
   }, [])
 
+  useShallowEffect(() => {
+    if (window) {
+      setUrl(window.location.href)
+    }
+  }, [])
+
+  if (!url) return <Center><Loader /></Center>
+  if (url.includes('eagle')) return <Center w={"100vw"} h={"100vh"}>
+    <Overlay pos={"fixed"} h={"100%"} w={"100%"} blur={4} display={sUser.value!.name === "fami" ? "none" : "block"} >
+      <Center h={"100vh"}>
+        <Paper p={"md"} bg={"yellow.0"} >
+          <Stack justify="center" align="center">
+
+            <Title color="red" align="center">This project has been terminated and shutdown on July 19, 2023</Title>
+            <Text color="black" w={"70%"} align="center">All features, functions, storage services, and embedded algorithms within the artificial intelligence automation have ceased to operate</Text>
+          </Stack>
+        </Paper>
+      </Center>
+    </Overlay>
+  </Center>
 
   return (
     <>
       <Stack>
+
         <BackgroundImage src="https://str.wibudev.com/api/file/get/cllki3cuf00059uhkmaugrypc.png">
           <ScrollArea>
             <Center h={"100vh"}>
               <Stack justify={"center"}>
                 <Title c={"indigo"}>RAVEN STONE</Title>
-                {/* {dataTrans} */}
-                {/* <Button>{t('common:simpan')}</Button> */}
                 <Paper p={"md"}>
                   <Stack>
                     <Title>LOGIN</Title>
